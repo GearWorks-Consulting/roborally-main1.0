df8079ba9b9c9a433c036c343c094e38c5a9e752
py2100 <abdisamad.nuh@outlook.com>
2023-06-17 23:31:20 +0200

    cleaned up product

--- roborally-main/roborally-1.2.0-java17-main/roborally-1.2.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/client/ProductClient.java
+++ roborally-main/roborally-1.2.0-java17-main/roborally-1.2.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/client/ProductClient.java
@@ -24,72 +24,11 @@ public class ProductClient implements IBoardTemplate {
             .connectTimeout(Duration.ofSeconds(10))
             .build();
 
-    public static String getProducts() throws Exception {
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("http://10.209.211.14:8080/products"))
-                .setHeader("User-Agent", "Product Client")
-                .header("Content-Type", "application/json")
-                .build();
-        CompletableFuture<HttpResponse<String>> response =
-                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-        String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
-        return result;
-    }
-
-    public static String getProduct() throws Exception {
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("http://localhost:8080/products/100"))
-                .setHeader("User-Agent", "Product Client")
-                .header("Content-Type", "application/json")
-                .build();
-        CompletableFuture<HttpResponse<String>> response =
-                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-        String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
-        return result;
-    }
 
 
-    public Product getProductById(int id) {
-        try{
-            HttpRequest request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(URI.create("http://localhost:8080/products/" + id))
-                    .setHeader("User-Agent", "Product Client")
-                    .header("Content-Type", "application/json")
-                    .build();
-            CompletableFuture<HttpResponse<String>> response =
-                    httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-            String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
-            Gson gson = new Gson();
-            Product p = gson.fromJson(result, Product.class);
-            return p;
-        } catch (Exception e) {
-            return null;
-        }
-    }
 
-    public static boolean setCompleteMove(String p) {
-        try{
-            String productJSON =p;
-            HttpRequest request = HttpRequest.newBuilder()
-                    .POST(HttpRequest.BodyPublishers.ofString(productJSON))
-                    .uri(URI.create("http://10.209.211.14:8080/setCompleteMove/"))
-                    .setHeader("User-Agent", "Product Client")
-                    .header("Content-Type", "application/json")
-                    .build();
-            CompletableFuture<HttpResponse<String>> response =
-                    httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-            String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
-            System.out.println("Great Success");
-            return result.equals("added")? true : false;
 
-        } catch (Exception e) {
-            System.out.println("Great Failure");
-            return false;
-        }
-    }
+
 
     public static boolean updatePlayerJoinedCounter(String p) {
         try{
@@ -209,82 +148,10 @@ public class ProductClient implements IBoardTemplate {
         }
     }
 
-    public static String printTest() {
-        try{
-            HttpRequest request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(URI.create("http://10.209.211.14:8080/printTest/" ))
-                    .setHeader("User-Agent", "Product Client")
-                    .header("Content-Type", "application/json")
-                    .build();
-            CompletableFuture<HttpResponse<String>> response =
-                    httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-            String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
-            return result;
-        } catch (Exception e) {
-            return null;
-        }
-    }
-    public static boolean isCompleteMove() {
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(URI.create("http://10.209.211.14:8080/isCompleteMove/"))
-                    .setHeader("User-Agent", "Product Client")
-                    .header("Content-Type", "application/json")
-                    .build();
-
-            CompletableFuture<HttpResponse<String>> response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-
-
-            boolean result = Boolean.parseBoolean(response.get(5, TimeUnit.SECONDS).body());
-
-            return result;
-        } catch (Exception e) {
-            return false;
-        }
-    }
 
 
 
 
-
-
-    public boolean updateProduct(int id, Product p) {
-        try{
-            String productJSON = new Gson().toJson(p);
-            HttpRequest request = HttpRequest.newBuilder()
-                    .PUT(HttpRequest.BodyPublishers.ofString(productJSON))
-                    .uri(URI.create("http://localhost:8080/products/" + id))
-                    .setHeader("User-Agent", "Product Client")
-                    .header("Content-Type", "application/json")
-                    .build();
-            CompletableFuture<HttpResponse<String>> response =
-                    httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-            String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
-            return result.equals("updated")? true : false;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-
-    public boolean deleteProductById(int id) {
-        try{
-            HttpRequest request = HttpRequest.newBuilder()
-                    .DELETE()
-                    .uri(URI.create("http://localhost:8080/products/" + id))
-                    .setHeader("User-Agent", "Product Client")
-                    .header("Content-Type", "application/json")
-                    .build();
-            CompletableFuture<HttpResponse<String>> response =
-                    httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-            String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
-            return result.equals("deleted")? true : false;
-        } catch (Exception e) {
-            return false;
-        }
-    }
 
 
 }
